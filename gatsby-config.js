6c8f027220d5e7eb3e7f2a523e9945092cd8d6df
BigBro222 <ze.qi@outlook.com>
2022-05-07 11:37:15 +0800

    custom personal

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,19 @@ require(`dotenv`).config()
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
+  pathPrefix: `/`,
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitle: `BigBro222's Blog`,
+    siteTitleAlt: `BigBro222's Blog `,
+    siteHeadline: `BigBro222's Blog `,
     siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteDescription: `Personal site for Ze Qi`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@Ze.Qi`,
   },
   plugins: [
     {
